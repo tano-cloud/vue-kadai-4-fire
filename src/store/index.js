4cb7ec5af73c050b28c61b0b31f06cb45ffee10f
tanobit <aklramewaruitano@gmail.com>
2020-10-07 04:07:53 +0900

    rev3

--- src/store/index.js
+++ src/store/index.js
@@ -7,14 +7,26 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   strict:true,
   state: {
+    userInfo:[]
   },
   mutations: {
-    mutationsRegisterUserInfo(state,payload){
+    mutationsRegisterUserInfo(state, payload){
+      state.userInfo.push({
+        email:payload.userInfo.email,
+        password:payload.userInfo.password,
+        name:payload.userInfo.name
+      })
+    }
+  },
+  actions: {
+    actionRegisterUserInfo(context, payload){
+      //payload=(email,password,name)
       firebase.auth().createUserWithEmailAndPassword(payload.userInfo.email, payload.userInfo.password).then((result) => {
         result.user.updateProfile({
             displayName: payload.userInfo.name
         }).then(() => {
             alert('アカウントの新規作成が完了しました！')
+            context.commit('mutationsRegisterUserInfo', payload)
         })
     },
     (error) => {
@@ -26,12 +38,6 @@ export default new Vuex.Store({
         }
       }
       )
-    }
-  },
-  actions: {
-    actionRegisterUserInfo(context,payload){
-      //payload=(email,password,name)
-      context.commit('mutationsRegisterUserInfo',payload)
     }
   },
 })
